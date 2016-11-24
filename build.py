ac0a619544b880fddd2086a0f5d2d1b8a0e7718a
David Terry <david.terry@native-instruments.de>
2016-11-25 00:05:49 +0100

    Add travis and appveyor integration

---
+++ build.py
@@ -0,0 +1,10 @@
+from conan.packager import ConanMultiPackager
+
+if __name__ == "__main__":
+    builder = ConanMultiPackager(
+        username="xwvvvvwx",
+        use_docker=True
+    )
+
+    builder.add_common_builds(pure_c=False)
+    builder.run()
