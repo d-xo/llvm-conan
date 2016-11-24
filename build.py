17eca9557f235590e42991caba787cb86c144cab
David Terry <david.terry@native-instruments.de>
2016-11-25 00:25:15 +0100

    Only build with docker on linux

--- build.py
+++ build.py
@@ -1,9 +1,10 @@
 from conan.packager import ConanMultiPackager
+from platform import system
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(
         username="xwvvvvwx",
-        use_docker=True
+        use_docker=True if system() == 'Linux' else False
     )
 
     builder.add_common_builds(pure_c=False)
