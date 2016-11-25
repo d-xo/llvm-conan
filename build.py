da84a6a5022d2a4bed50de58b95e256defc7064c
David Terry <david.terry@native-instruments.de>
2016-11-25 09:14:21 +0100

    dont use docker for linux builds

--- build.py
+++ build.py
@@ -3,8 +3,7 @@ from platform import system
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(
-        username="xwvvvvwx",
-        use_docker=True if system() == 'Linux' else False
+        username="xwvvvvwx"
     )
 
     builder.add_common_builds(pure_c=False)
